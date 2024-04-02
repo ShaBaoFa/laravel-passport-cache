6dce12c474e8032234ec7a2c88620177e1fa5eb4
ShaBaoFa <wlfpanda1012@gmail.com>
2024-04-02 17:26:16 +0800

    fix bug

--- src/CacheServiceProvider.php
+++ src/CacheServiceProvider.php
@@ -27,7 +27,7 @@ class CacheServiceProvider extends ServiceProvider
             );
         });
         $this->app->singleton(ClientRepository::class, function () {
-            return new CacheTokenRepository(
+            return new CacheClientRepository(
                 Config::get('passport.cache.client.prefix'),
                 Config::get('passport.cache.client.expires_in'),
                 Config::get('passport.cache.client.tags', []),
